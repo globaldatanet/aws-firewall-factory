8fcce8c4424481ffe7eb53627c147d0aeb0dafaf
daknhh <david@globaldatanet.com>
2023-06-27 22:30:56 +0200

    fix

--- lib/tools/config-validator.ts
+++ lib/tools/config-validator.ts
@@ -1,10 +1,5 @@
 import Ajv, {JSONSchemaType} from "ajv";
-<<<<<<< HEAD
 import { Config , Prerequisites, IPSet} from "../types/config";
-=======
-import { Config , Prerequisites} from "../types/config";
-import { IPSet } from "../types/ipset";
->>>>>>> 6e7db7ab (Add repo managed IPSets + refactor bin)
 import { resolve } from "path";
 import * as TJS from "typescript-json-schema";
 
@@ -22,28 +17,17 @@ const program = TJS.getProgramFromFiles(
   compilerOptions
 );
 
-<<<<<<< HEAD
-=======
-const ipsets = TJS.getProgramFromFiles(
-  [resolve("lib/types/ipset.ts")],
-  compilerOptions
-);
->>>>>>> 6e7db7ab (Add repo managed IPSets + refactor bin)
-
 const wafschema = TJS.generateSchema(program, "Config", settings);
 
 const prerequisitesschema = TJS.generateSchema(program, "Prerequisites", settings);
 
-<<<<<<< HEAD
 const ipSetsSchema = TJS.generateSchema(program, "IPSet", settings);
-=======
-const ipSetsSchema = TJS.generateSchema(ipsets, "IPSet", settings);
->>>>>>> 6e7db7ab (Add repo managed IPSets + refactor bin)
+
 
 const ajv = new Ajv();
 
-export const validatewaf = ajv.compile(wafschema as JSONSchemaType<Config>);
+export const validateWaf = ajv.compile(wafschema as JSONSchemaType<Config>);
 
-export const validateIPSets = ajv.compile(ipSetsSchema as JSONSchemaType<IPSet>);
+export const validateIpSets = ajv.compile(ipSetsSchema as JSONSchemaType<IPSet>);
 
-export const validateprerequisites = ajv.compile(prerequisitesschema as JSONSchemaType<Prerequisites>);
+export const validatePrerequisites = ajv.compile(prerequisitesschema as JSONSchemaType<Prerequisites>);
