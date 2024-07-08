93dcdb7cac83e912ab6a3e2df11284853ed96757
daknhh <hamburger1992@googlemail.com>
2024-07-08 16:50:46 +0200

    update examples

--- values/examples/shield-advanced.ts
+++ values/examples/shield-advanced.ts
@@ -6,6 +6,7 @@ export const shieldConfig: ShieldConfig = {
     S3LoggingBucketName: "aws-waf-logs-aws-firewall-factory-test",
     LoggingConfiguration: "S3",
     SecuredDomain: ["test.aws-firewall-factory.com"],
+    CreateDashboard: true,
   },
   WebAcl: { OverrideCustomerWebACLAssociation: false },
   defaultActionType:"ALLOW",
