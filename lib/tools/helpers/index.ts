f4d7e57e0d38f1754d75e46de0fca2fa79ddd563
daknhh <hamburger1992@googlemail.com>
2024-09-02 16:14:13 +0200

    change docs

--- lib/tools/helpers/index.ts
+++ lib/tools/helpers/index.ts
@@ -4,17 +4,17 @@
  *
  * This Helper Functions are used to provide the necessary functions to the AWS Firewall Factory.
  * ## 🧰 Helpers:
- * AWS Firewall Factory Helper (afwfHelper): Contains the necessary functions to print firewall factory information, etc.
- * 
- * CloudFormation Helper (cloudformationHelper): Contians the necessary functions to get CloudFormation Stacks output etc.
- * 
- * Web Application Firewall Helper (wafHelper): Contains the necessary functions to calculate WCUs for WebACLs, get Managed RuleGroups versions, etc.
- * 
- * Pricing Helper (pricingHelper): Contains the necessary functions to get pricing information for WAF and Advanced Shield.
- * 
- * Guidance Helper (guidanceHelper): Provides the necessary functions to get the guidance for AWS Firewall Factory that is printed to the console during deployment.
- * 
- * SSM Helper (ssmHelper): Provides the necessary functions to retrieve parameters, etc. from the AWS SSM Parameter Store.
+ * - AWS Firewall Factory Helper (afwfHelper): Contains the necessary functions to print firewall factory information, etc.
+ *
+ * - CloudFormation Helper (cloudformationHelper): Contians the necessary functions to get CloudFormation Stacks output etc.
+ *
+ * - Web Application Firewall Helper (wafHelper): Contains the necessary functions to calculate WCUs for WebACLs, get Managed RuleGroups versions, etc.
+ *
+ * - Pricing Helper (pricingHelper): Contains the necessary functions to get pricing information for WAF and Advanced Shield.
+ *
+ * - Guidance Helper (guidanceHelper): Provides the necessary functions to get the guidance for AWS Firewall Factory that is printed to the console during deployment.
+ *
+ * - SSM Helper (ssmHelper): Provides the necessary functions to retrieve parameters, etc. from the AWS SSM Parameter Store.
 */
 export * as cloudformationHelper from "./cloudformation";
 export * as wafHelper from "./web-application-firewall";
